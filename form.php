d44eb5511013d7523f373a9a5a6b882b5cc816e7
Apinan Woratrakun <iamapinan@gmail.com>
2015-01-31 22:16:16 +0700

    fix form_element.json url

--- form.php
+++ form.php
@@ -8,7 +8,7 @@ class form {
   private $form_element;
 
   function __construct(){
-    $this->form_element = json_decode(file_get_contents('../metadata/form_element.json'), true);
+    $this->form_element = json_decode(file_get_contents('form_element.json'), true);
   }
 
   public function form_start($method="POST", $action=null, $attribute=null){
